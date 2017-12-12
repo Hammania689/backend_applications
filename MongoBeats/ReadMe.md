d6488a810980b38d0496bfb35d185d671c44cff9
Hammania689 <hameedabdulrashid@gmail.com>
2017-12-11 23:08:27 -0600

    Captured Text value and passed it to the next page.

---
+++ MongoBeats/ReadMe.md
@@ -0,0 +1,3 @@
+End Project goal is a Express driven application that queries Mongodb database for login.
+
+
--- MongoBeats/server.js
+++ MongoBeats/server.js
@@ -1,24 +1,25 @@
+// Required libraries for application
 const express = require("express");
 const mongodb = require("mongodb");
 const ejs = require("ejs");
 const bodyParser = require('body-parser');
 const mongoose = require("mongoose");
 
-
+// Express object handler
 const app = express();
 
+// Ejs enables express to drive data rendered on front end
 app.set("view engine", "ejs");
 
+// Body Parser and Static view to capture text and render static webpages
 app.use(express.static('views'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
+// Use to connect to db
 var promise = mongoose.connect('mongodb://localhost/backend_fun', {
     useMongoClient: true,
-    /* other options */
   });
-//mongoose.connect("mongodb://localhost/backend_fun");
-
 
 var personSchema = new mongoose.Schema({
     name : String,
@@ -27,24 +28,36 @@ var personSchema = new mongoose.Schema({
 
 var User = mongoose.model("User", personSchema);
 
+var isInDataBase = (name,age) =>
+{
 
+}
+
+// Routes for pages
+app.get('/',(req,res)=> res.render('index.ejs'));
 
 app.post('/',(request,response,next) => {
     
     var name = request.body.name;
     var age = request.body.age;
-    var myData = new User(request.body);
-    myData.save();
+
     
-    if(myData.save())
-        console.log("saved " + name + " , " + age + " to database");
-    else 
-        console.log("failed to add to db..");
-    response.redirect('/');
+    if(isInDataBase(name,age))
+        response.render("profile.ejs", {name : name, age : age});
+    else
+    {
+        var myData = new User(request.body);
+        myData.save();
+
+        if(myData.save())
+            console.log("saved " + name + " , " + age + " to database");
+        else 
+            console.log("failed to add to db..");
+    }  
+
     next();
 });
 
-app.get('/',(req,res)=> res.render('index.ejs'));
 
 app.listen(8080, ()=> console.log('Threat Neutralized'));
 
